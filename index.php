7b185cb46cb26d3597c2e93744df2d262718b01a
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2018-10-15 14:55:32 -0400

    testing heroku connection

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bracket Referee</title>
+  </head>
+  <body>
+    This is a test for Heroku.
+  </body>
+</html>
