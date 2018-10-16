fa28b57fa41bf51f51bd1539b7f710f7e4bdcecb
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2018-10-16 16:37:48 -0400

    started index page

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Bracket Referee</title>
+    <title>
+      Bracket Referee
+    </title>
   </head>
   <body>
     This is a test for Heroku.
