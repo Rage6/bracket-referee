a52bcbb1f438a967ee0ed2f9d9495c93240bf6ae
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2018-10-21 01:52:54 -0400

    port changed to 3306

--- pdo.php
+++ pdo.php
@@ -2,6 +2,6 @@
   // For local host with MAMP...
   // $pdo = new PDO('mysql:host=localhost;port=8888;dbname=Bracket_Referee','Nick','Ike');
   // For ClearDB with Heroku...
-  $pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=8888;dbname=heroku_09cdd5d6c600012','bff92f85b9436f','d944cc2a');
+  $pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_09cdd5d6c600012','bff92f85b9436f','d944cc2a');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
