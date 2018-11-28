edd4003934b454e76d00ede5107bff52e0b1777c
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2018-11-27 20:16:43 -0800

    local commit

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 // For local host with MAMP...
-// $pdo = new PDO('mysql:host=localhost;port=8888;dbname=Bracket_Referee','Nick','Ike');
+$pdo = new PDO('mysql:host=localhost;port=8888;dbname=Bracket_Referee','Nick','Ike');
 // For ClearDB with Heroku...
-$pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_09cdd5d6c600012','bff92f85b9436f','21369ba7a112329');
+// $pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_09cdd5d6c600012','bff92f85b9436f','passwordGoesHere');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
