03ac9f0cbfc10521e6a9a8fb43db75fa358df562
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2019-01-16 20:09:59 -0500

    cleared old password

--- pdo.php
+++ pdo.php
@@ -5,7 +5,7 @@ $currentHost = $_SERVER['HTTP_HOST'];
 if ($currentHost == 'localhost:8888') {
   $pdo = new PDO('mysql:host=localhost;port=8888;dbname=Bracket_Referee','Nick','Ike');
 } else {
-  $pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_09cdd5d6c600012','bff92f85b9436f','98b148e13adade7');
+  $pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_09cdd5d6c600012','bff92f85b9436f','*passwrd_goes_here*');
 };
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
