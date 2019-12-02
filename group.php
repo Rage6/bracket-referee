cf90813124d189c11386b026fd8d130d86bf5c1c
Nicholas Vogt <nicholasvogt@Nicholass-MacBook-Pro.local>
2019-12-02 13:11:48 -0800

    ACTUALLY fixed the EST

--- group.php
+++ group.php
@@ -349,7 +349,7 @@
             <td class="rowValue">
               <?php
                 echo(date('m/d/Y', $tournArray['selection_date']))
-              ?> EST
+              ?>
             </td>
           </tr>
           <tr>
@@ -357,7 +357,7 @@
             <td class="rowValue">
               <?php
                 echo(date('m/d/Y g:ia', $tournArray['start_date']))
-              ?>
+              ?> EST
             </td>
           </tr>
           <tr>
