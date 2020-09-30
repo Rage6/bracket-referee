99f691e2763086c6994bf5746243abc014e0b3ef
Nicholas Vogt <nicholasvogt@Nicholass-MBP.lan>
2020-09-29 22:47:08 -0400

    switched back to old pdo setup

--- pdo.php
+++ pdo.php
@@ -5,8 +5,8 @@ $currentHost = $_SERVER['HTTP_HOST'];
 if ($currentHost == 'localhost:8888') {
   $pdo = new PDO('mysql:host=localhost;port=8888;dbname=Bracket_Referee','Nick','Ike');
 } else {
-  $pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_09cdd5d6c600012','bff92f85b9436f',$_ENV['CLEARDB_DATABASE_PASSWORD']);
-  $apiKey = $_ENV['SENDGRID_API_KEY'];
+  $pdo = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;port=3306;dbname=heroku_09cdd5d6c600012','bff92f85b9436f','*passwrd_goes_here*');
+  $apiKey = '*api_key_goes_here*';
 };
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
